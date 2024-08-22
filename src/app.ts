564249b915cefb016c0815ae94966eb23849df17
Wendel Tavares <wendeljt17@gmail.com>
2024-08-22 19:00:39 -0300

    create table with docker

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from "fastify";
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient() //Instanciando o BD
+
+prisma.user.create({
+    data: {
+        nome: 'Wendel Tavares',
+        email: 'wendel@exemplo.com'
+    }
+})
