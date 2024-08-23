c7e1e53f181ebeabec415fdc8ca8edaba5465c6c
Wendel Tavares <wendeljt17@gmail.com>
2024-08-23 10:09:07 -0300

    cryptograph password

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,7 @@
 import { FastifyReply, FastifyRequest } from "fastify"
 import { z } from "zod"
 import { prisma } from "../../lib/prisma"
+import { hash } from 'bcryptjs'
 
 export async function register(req: FastifyRequest, rep: FastifyReply) {
     const registerBodySchema = z.object({
@@ -11,11 +12,23 @@ export async function register(req: FastifyRequest, rep: FastifyReply) {
 
     const { nome, email, password} = registerBodySchema.parse(req.body)
 
+    const password_hash = await hash(password, 6) //Criptografando a senha
+
+    const userWithSameEmail = await prisma.user.findUnique({ //Verifica se existe um email criado
+        where: {
+            email
+        }
+    })
+
+    if(userWithSameEmail){
+        return rep.status(409).send()
+    }
+
     await prisma.user.create({
         data: {
             nome,
             email,
-            password_hash: password
+            password_hash
         }
     })
 
