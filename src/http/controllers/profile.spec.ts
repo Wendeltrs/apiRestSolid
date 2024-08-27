e2df89ded2410c73f34a71a49516940e1b1a90bf
Wendel Tavares <wendeljt17@gmail.com>
2024-08-27 12:42:29 -0300

    profile's test e2e

---
+++ src/http/controllers/profile.spec.ts
@@ -0,0 +1,36 @@
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import request from 'supertest'
+import { app } from "../../app";
+
+describe('Profile (e2e', () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
+    afterAll(async () => {
+        await app.close()
+    })
+
+    it('Should be able to get user profile', async () => {
+        await request(app.server).post('/users').send({
+            nome: 'Wendel Tavares',
+            email: 'wendel@example.com',
+            password: '123456'
+        })
+
+        const authenticateResponse = await request(app.server).post('/sessions').send({
+            email: 'wendel@example.com',
+            password: '123456'
+        })
+
+        const { token } = authenticateResponse.body
+
+        const profileResponse = await request(app.server).get('/me').set('Authorization', `Bearer ${token}`).send()
+
+        expect(profileResponse.status).toEqual(200)
+        expect(profileResponse.body.user).toEqual(
+            expect.objectContaining({
+                email: 'wendel@example.com'
+            })
+        )
+    })
+})
