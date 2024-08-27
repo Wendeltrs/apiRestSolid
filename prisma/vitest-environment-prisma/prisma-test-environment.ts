be3733d6345c25fa3a727bb74e10a5835498d4eb
Wendel Tavares <wendeljt17@gmail.com>
2024-08-27 12:00:49 -0300

    prisma test environment

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,15 +1,38 @@
+import { PrismaClient } from '@prisma/client'
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
 import { Environment } from 'vitest/environments'
 
-export default <Environment>{ 
+const prisma = new PrismaClient()
+
+function generateDatabaseURL(schema: string){
+    if(!process.env.DATABASE_URL){
+        throw new Error('Please provide a DATABASE_URL environment variables!')
+    }
+
+    const url = new URL(process.env.DATABASE_URL)
+
+    url.searchParams.set('schema', schema)
+
+    return url.toString()
+}
+
+export default <Environment>{  //postgresql://docker:docker@localhost:5432/apisolid?schema=public
     name: 'prisma',
     transformMode: 'ssr',
-    async setup(){
-        console.log('Setup!') //Executa antes de cada teste
+    async setup(){ //Executa antes de cada teste
+        const schema = randomUUID()
+        const databaseURL = generateDatabaseURL(schema)
+        
+        process.env.DATABASE_URL = databaseURL
+
+        execSync('prisma migrate deploy')
 
         return {
-            teardown(){
-                'Teardown'
-            } //Executa depois de cada teste
+            teardown(){ //Executa depois de cada teste
+                prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schema}" CASCADE`)
+            } 
         }
     }
 }
